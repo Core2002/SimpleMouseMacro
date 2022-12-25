b7168373133c71a68123e0d9de5a0afaf7341f08
NekokeCore <Core2002@163.com>
2022-12-25 21:35:30 +0800

    实现了连点器

---
+++ AutoClick/AutoClick.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() {
+	SetConsoleTitle(L"CPS - 28");
+	std::cout << "按住鼠标侧键不放自动连点" << std::endl;
+	int delay = 15;
+
+	while (true)
+	{
+		if (GetAsyncKeyState(VK_XBUTTON2))
+		{
+			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+			Sleep(5);
+			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+		}
+		Sleep(delay);
+	}
+}
